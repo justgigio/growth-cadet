33d2db71438fdbc431a530019481ba2c640bae3a
Giovane Costa <gigio.coder@gmail.com>
2024-03-21 18:17:19 -0300

    First endpoint working

--- app/controllers/api/v1/dns_records_controller.rb
+++ app/controllers/api/v1/dns_records_controller.rb
@@ -8,7 +8,26 @@ module Api
 
       # POST /dns_records
       def create
-        # TODO: Implement this action
+        payload = params.require(:dns_records).permit(:ip, hostnames_attributes: [:hostname])
+
+        hostnames = payload[:hostnames_attributes].map { |hn| Dns::Record.find_or_initialize_by(*hn) }
+        address = Dns::Address.find_or_initialize_by(ipv4: payload[:ip])
+
+        if address.valid? and hostnames.all?(&:valid?)
+          Dns::Address.transaction do
+            address.records << hostnames.difference(address.records)
+            address.save
+          end
+
+          json_response = {id: address.id}
+          render json: json_response
+        else
+          json_response = {errors: {
+            dns_records: address.errors,
+            hostnames_attributes: hostnames.map(&:errors)
+          }}
+          render json: json_response, status: :unprocessable_entity
+        end
       end
     end
   end
