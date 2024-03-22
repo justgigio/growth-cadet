bc7cc361ebf59b794432a33cbe66bafb8c2e231d
Giovane Costa <gigio.coder@gmail.com>
2024-03-22 02:43:32 -0300

    Change DatabaseCleaner behavior to truncation in order to reset ids

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,7 +44,7 @@ RSpec.configure do |config|
   end
 
   config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each, js: true) do
