6567d1d66c29228fc350ff25aacafcd13369b559
Giovane Costa <gigio.coder@gmail.com>
2024-03-21 22:47:08 -0300

    Second endpoint working

--- app/controllers/api/v1/dns_records_controller.rb
+++ app/controllers/api/v1/dns_records_controller.rb
@@ -3,7 +3,59 @@ module Api
     class DnsRecordsController < ApplicationController
       # GET /dns_records
       def index
-        # TODO: Implement this action
+        # must come from config
+        records_per_page = 10
+
+        query_string = params.permit(:page, :included, :excluded)
+        page = query_string.require(:page)
+
+        addresses_query = Dns::Address.joins(:records)
+        included = []
+        excluded = []
+
+        if query_string[:included].present?
+          included = query_string[:included].split(',')
+          addresses_query = addresses_query.where(records: { hostname: included })
+          addresses_query = addresses_query.group('dns_addresses.id').having('COUNT(dns_addresses.id) >= ?', included.length)
+        end
+
+        if query_string[:excluded].present?
+          excluded = query_string[:excluded].split(',')
+          excluded_ids = Dns::Address.joins(:records).where(records: { hostname: excluded }).pluck(:id)
+          addresses_query = addresses_query.where.not(dns_addresses: { id: excluded_ids })
+        end
+
+        count = addresses_query.distinct(:id).count.count
+
+        limit = records_per_page
+        offset = records_per_page * (page.to_i - 1)
+
+        addresses_query = addresses_query.limit(limit).offset(offset)
+
+        records = []
+
+        addresses_query.each do |address|
+          records << { id: address.id, ip_address: address.ipv4 }
+        end
+
+        records_query = Dns::Record.joins(:addresses).where(addresses: {id: addresses_query.pluck(:id)})
+
+        related_hostnames = {}
+        queried_hostnames = included + excluded
+
+        records_query.each do |record|
+          unless queried_hostnames.include?(record.hostname)
+            related_hostnames[record.hostname] ||= { hostname: record.hostname, count: 0 }
+            related_hostnames[record.hostname][:count] += 1
+          end
+        end
+
+        json_response = {
+          total_records: count,
+          records: records,
+          related_hostnames: related_hostnames.values
+        }
+        render json: json_response
       end
 
       # POST /dns_records
