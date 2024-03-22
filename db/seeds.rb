a76231832dcc368dd9f7335b40bba8b84d1b8ada
Giovane Costa <gigio.coder@gmail.com>
2024-03-21 22:46:32 -0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dns::Address.all.each(&:destroy)
+Dns::Record.delete_all
+
+lorem = Dns::Record.create(hostname: "lorem.com")
+ipsum = Dns::Record.create(hostname: "ipsum.com")
+dolor = Dns::Record.create(hostname: "dolor.com")
+amet = Dns::Record.create(hostname: "amet.com")
+sit = Dns::Record.create(hostname: "sit.com")
+
+ones = Dns::Address.create(ipv4: "1.1.1.1")
+ones.records << [lorem, ipsum, dolor, amet]
+
+twos = Dns::Address.create(ipv4: "2.2.2.2")
+twos.records << [ipsum]
+
+threes = Dns::Address.create(ipv4: "3.3.3.3")
+threes.records << [ipsum, dolor, amet]
+
+fours = Dns::Address.create(ipv4: "4.4.4.4")
+fours.records << [sit, ipsum, dolor, amet]
+
+fives = Dns::Address.create(ipv4: "5.5.5.5")
+fives.records << [sit, dolor]
